4a9addd6e396b3b5308ed6a6a810df54af1c9b68
Dunya Kirkali <dunyakirkali@gmail.com>
2025-12-23 22:45:44 +0100

    feat: switch tree-sitter

--- languages/asciidoc/highlights.scm
+++ languages/asciidoc/highlights.scm
@@ -1,168 +1,248 @@
-(document_title) @title @variant
+; =========================
+; Comments
+; =========================
+(comment) @comment
 
-[
-    (title1)
-    (title2)
-    (title3)
-    (title4)
-    (title5)
-] @title
 
-(email) @link_uri @link_text
+; =========================
+; Headings & titles
+; =========================
+(document_title_marker) @title @variant
+(document_title_text)   @title @variant
 
-(author_line
-  ";" @punctuation.delimiter)
+(section_marker_2) @title
+(section_marker_3) @title
+(section_marker_4) @title
+(section_marker_5) @title
+(section_marker_6) @title
 
-(revision_line
-  "," @punctuation.delimiter
-  ":" @punctuation.delimiter)
+(title)       @title
+(block_title) @title
 
-(list_continuation) @constant
+(author_name)         @string
+(author_email)        @string
+(revision_version)    @number
+(revision_date)       @number
+(revision_remark)     @string
 
-[
-  (firstname)
-  (middlename)
-  (lastname)
-] @attribute
 
-(revnumber) @number
+; =========================
+; Attribute entries
+; =========================
+(attribute_name)  @property
+(attribute_value) @string
 
-(revdate) @string.special
+(source_block_attributes) @attribute
+(source_attribute_keyword) @keyword
+(source_language) @type
 
-(revremark) @string
 
-(table_block_marker) @punctuation.special
+; =========================
+; Attribute substitutions
+; =========================
+(attribute_substitution
+  (plain_left_brace)  @punctuation.bracket
+  (plain_right_brace) @punctuation.bracket)
 
-(table_cell_attr) @attribute
+; {name}
+(attribute_substitution
+  (plain_text) @constant)
+
+; {ns:name}
+(attribute_substitution
+  (plain_text)  @constant
+  (plain_colon) @punctuation.delimiter
+  (plain_text)  @constant)
+
+
+; =========================
+; Admonitions
+; =========================
+(admonition_label) @keyword
+
+(inline_admonition
+  (admonition_label) @keyword
+  (plain_colon)       @punctuation.delimiter)
+
+
+; =========================
+; Lists + continuation
+; =========================
+(unordered_list_marker) @punctuation.list_marker
+(ordered_list_marker)   @punctuation.list_marker
+(checklist_marker)      @punctuation.list_marker
+(LIST_CONTINUATION)     @punctuation.list_marker
+
+(callout_marker) @punctuation.list_marker
+
+
+; =========================
+; Description lists
+; =========================
+; (description_list) @none
+
+(description_item_term)       @punctuation.list_marker
+(description_item_definition) @punctuation.list_marker
+
+
+; =========================
+; Callouts
+; =========================
+(callout_marker) @punctuation.list_marker
+
+
+; =========================
+; Links / xrefs / anchors
+; =========================
+(auto_link) @link_uri
+
+(explicit_link
+  (auto_link) @link_uri
+  (plain_left_bracket)  @punctuation.bracket
+  ((link_text) @link_text)?
+  (plain_right_bracket) @punctuation.bracket)
+
+(internal_xref
+  "<<" @punctuation.special
+  (xref_target) @link_text
+  ((plain_comma) @punctuation.delimiter (xref_text) @link_text)?
+  ">>" @punctuation.special)
+
+(inline_anchor) @link_text
+(anchor)        @link_text
+
+(bibliography_entry) @link_text
+; (bibliography_entry
+  ; (bibliography_id) @definition.label)
+(bibliography_entry
+  (bibliography_text) @string)
+
+
+; =========================
+; Inline formatting
+; =========================
+(strong)       @emphasis.strong
+(emphasis)     @emphasis
+; (monospace)    @markup.raw
+; (superscript)  @markup.superscript
+; (subscript)    @markup.subscript
+; (highlight)    @markup.highlight
+
+; (passthrough_triple_plus) @markup.raw
+
+(role_attribute_list) @attribute
+
+; =========================
+; Punctuation
+; =========================
+(plain_dot) @punctuation.delimiter
+
+
+; =========================
+; Macros
+; =========================
+(macro_name)  @function
+(macro_body)  @string
+(macro_close) @punctuation.bracket
+
+(block_macro (macro_name) @function)
+(inline_macro (macro_name) @function)
+
+
+; =========================
+; Index terms
+; =========================
+(index_term_macro)     @keyword
+(index_term2_macro)    @keyword
+(concealed_index_term) @keyword
+
+(index_text)      @string
+(index_term_text) @string
+
+"indexterm:"  @keyword
+"indexterm2:" @keyword
+"((("         @punctuation.special
+")))"         @punctuation.special
+
+
+; =========================
+; Conditionals / directives
+; =========================
+(ifdef_open)       @keyword
+(ifndef_open)      @keyword
+(ifeval_open)      @keyword
+(endif_directive)  @keyword
+
+(conditional_block) @keyword
+(ifdef_block)       @keyword
+(ifndef_block)      @keyword
+(ifeval_block)      @keyword
+
+
+; =========================
+; Blocks: fences / markers
+; =========================
+(EXAMPLE_FENCE_START) @punctuation.special
+(EXAMPLE_FENCE_END)   @punctuation.special
+
+(LISTING_FENCE_START) @punctuation.special
+(LISTING_FENCE_END)   @punctuation.special
+
+(LITERAL_FENCE_START) @punctuation.special
+(LITERAL_FENCE_END)   @punctuation.special
+
+(QUOTE_FENCE_START) @punctuation.special
+(QUOTE_FENCE_END)   @punctuation.special
+
+(SIDEBAR_FENCE_START) @punctuation.special
+(SIDEBAR_FENCE_END)   @punctuation.special
+
+(PASSTHROUGH_FENCE_START) @punctuation.special
+(PASSTHROUGH_FENCE_END)   @punctuation.special
+
+(OPENBLOCK_FENCE_START) @punctuation.special
+(OPENBLOCK_FENCE_END)   @punctuation.special
+
+(TABLE_FENCE_START) @punctuation.special
+(TABLE_FENCE_END)   @punctuation.special
+
+(fenced_code_block_delimiter) @punctuation.special
+(FENCED_CODE_CONTENT_LINE)    @string
+; (block_quote_marker) @markup.quote
+
+(THEMATIC_BREAK) @punctuation.special
+("<<<")          @punctuation.special
+
+
+; =========================
+; Tables
+; =========================
+(table_attributes) @attribute
+(block_attributes) @attribute
 
 (table_cell
-  "|" @punctuation.special)
+  "|" @punctuation.delimiter)
 
-[
-  (breaks)
-  (hard_wrap)
-  (quoted_block_md_marker)
-  (quoted_paragraph_marker)
-  (open_block_marker)
-  (listing_block_start_marker)
-  (listing_block_end_marker)
-  (literal_block_marker)
-  (passthrough_block_marker)
-  (quoted_block_marker)
-  (ntable_block_marker)
-  (callout_marker)
-] @punctuation.special
+(cell_spec) @attribute
 
-(ntable_cell
-  "!" @punctuation.special)
 
-; (checked_list_marker_unchecked) @markup.list.unchecked
+; =========================
+; General punctuation
+; =========================
+(plain_left_brace)    @punctuation.bracket
+(plain_right_brace)   @punctuation.bracket
+(plain_left_bracket)  @punctuation.bracket
+(plain_right_bracket) @punctuation.bracket
+(plain_left_paren)    @punctuation.bracket
+(plain_right_paren)   @punctuation.bracket
+(plain_colon)         @punctuation.delimiter
+(plain_less_than)     @punctuation.bracket
+(plain_greater_than)  @punctuation.bracket
+(plain_caret)         @punctuation.special
+(plain_dash)          @punctuation.delimiter
+(plain_double_quote)  @punctuation.special
+(plain_quote)         @punctuation.special
+(plain_underscore)    @punctuation.delimiter
+(plain_asterisk)      @punctuation.special
 
-; (checked_list_marker_checked) @markup.list.checked
-
-; [
-;   (list_marker_star)
-;   (list_marker_hyphen)
-;   (list_marker_dot)
-;   (list_marker_digit)
-;   (list_marker_geek)
-;   (list_marker_alpha)
-; ] @markup.list
-
-[
-  (line_comment)
-  (block_comment)
-] @comment ; @spell
-
-[
-  (document_attr_marker)
-  (element_attr_marker)
-] @punctuation.delimiter
-
-(document_attr
-  (attr_name) @property)
-
-(element_attr
-  (attr_value) @attribute)
-
-(block_title
-  (block_title_marker) @punctuation.special) @attribute
-
-; (ident_block) @markup.raw.block
-
-(callout_list_marker) @punctuation.special
-
-; (block_macro
-;   (block_macro_name) @keyword
-;   "::" @punctuation.delimiter
-;   (target)? @markup.link
-;   "[" @punctuation.bracket
-;   "]" @punctuation.bracket)
-
-(attribute_name) @attribute
-
-; (attribute_value) @variable.parameter
-
-; (admonition
-;   (admonition_important) @comment.error
-;   ":" @comment.error)
-
-; (admonition
-;   (admonition_warning) @comment.warning
-;   ":" @comment.warning)
-
-; (admonition
-;   (admonition_caution) @comment.warning
-;   ":" @comment.warning)
-
-; (admonition
-;   (admonition_note) @comment.note
-;   ":" @comment.note)
-
-; (admonition
-;   (admonition_tip) @comment.note
-;   ":" @comment.note)
-
-; ((section_block
-;   (element_attr
-;     (element_attr_marker) @comment.note
-;     (attr_value) @attribute @comment.note
-;     (element_attr_marker) @comment.note)
-;   (delimited_block
-;     (delimited_block_start_marker) @comment.note
-;     (delimited_block_end_marker) @comment.note))
-;   (#any-of? @attribute "NOTE" "TIP"))
-
-; ((section_block
-;   (element_attr
-;     (element_attr_marker) @comment.warning
-;     (attr_value) @attribute @comment.warning
-;     (element_attr_marker) @comment.warning)
-;   (delimited_block
-;     (delimited_block_start_marker) @comment.warning
-;     (delimited_block_end_marker) @comment.warning))
-;   (#any-of? @attribute "CAUTION" "WARNING"))
-
-; ((section_block
-;   (element_attr
-;     (element_attr_marker) @comment.error
-;     (attr_value) @attribute @comment.error
-;     (element_attr_marker) @comment.error)
-;   (delimited_block
-;     (delimited_block_start_marker) @comment.error
-;     (delimited_block_end_marker) @comment.error))
-;   (#eq? @attribute "IMPORTANT"))
-
-; ((element_attr
-;   (element_attr_marker)
-;   (attr_value) @_attr
-;   (element_attr_marker))
-;   (listing_block
-;     (listing_block_body) @markup.raw.block)
-;   (#not-lua-match? @_attr "^[^,]*,%s*(%w+)")
-;   (#not-any-of? @_attr
-;     "a2s" "barcode" "blockdiag" "bpmn" "bytefield" "d2" "dbml" "diagrams" "ditaa" "dpic" "erd"
-;     "gnuplot" "graphviz" "lilypond" "meme" "mermaid" "msc" "nomnoml" "pikchr" "plantuml" "shaape"
-;     "smcat" "structurizr" "svgbob" "symbolator" "syntrax" "tikz" "umlet" "vega" "wavedrom"))
-
-; (line) @spell
+(hard_break) @punctuation.special
