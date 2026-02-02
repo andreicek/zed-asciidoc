3c2f93f40b1b2dfc917cde7febdfd558cd5bd2fc
Dunya Kirkali <dunyakirkali@gmail.com>
2026-02-03 00:02:48 +0100

    Add folds

---
+++ languages/asciidoc/folds.scm
@@ -0,0 +1,29 @@
+[
+  ; Sections
+  (document_title)
+  (title1)
+  (title2)
+  (title3)
+  (title4)
+  (title5)
+
+  ; Delimited blocks
+  (listing_block)
+  (literal_block)
+  (delimited_block)
+  (open_block)
+  (quoted_block)
+  (quoted_md_block)
+  (passthrough_block)
+  (block_comment)
+
+  ; Tables
+  (table_block)
+
+  ; Lists
+  (ordered_list)
+  (unordered_list)
+  (checked_list)
+  (callout_list)
+  (admonition)
+] @fold
