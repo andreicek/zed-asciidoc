6a9aa4f698c9295bee8024f9fe6d9d64b1594ca0
Dunya Kirkali <dunyakirkali@gmail.com>
2026-02-03 22:33:11 +0100

    Correct outline

--- languages/asciidoc/outline.scm
+++ languages/asciidoc/outline.scm
@@ -1,19 +1,29 @@
-;; Document title
-(document_title) @item @name
-(#set! kind "file")
+;; Document title (level 0)
+(document_title
+  (title_h0_marker) @context
+  (line) @name) @item
 
-;; Section titles (levels 1–5)
-(title1) @item @name
-(#set! kind "section")
+;; Section title level 1
+(title1
+  (title_h1_marker) @context
+  (line) @name) @item
 
-(title2) @item @name
-(#set! kind "section")
+;; Section title level 2
+(title2
+  (title_h2_marker) @context
+  (line) @name) @item
 
-(title3) @item @name
-(#set! kind "section")
+;; Section title level 3
+(title3
+  (title_h3_marker) @context
+  (line) @name) @item
 
-(title4) @item @name
-(#set! kind "section")
+;; Section title level 4
+(title4
+  (title_h4_marker) @context
+  (line) @name) @item
 
-(title5) @item @name
-(#set! kind "section")
+;; Section title level 5
+(title5
+  (title_h5_marker) @context
+  (line) @name) @item
